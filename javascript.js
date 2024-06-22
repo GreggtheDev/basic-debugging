a52c12c8b4be2c2283b3f7ca849539fbd51f06e0
GreggtheDev <greggthedev@gmail.com>
2024-06-21 21:22:11 -0400

    finished product (crosses fingers)

--- javascript.js
+++ javascript.js
@@ -1,35 +1,32 @@
+// Each store owner is an object with properties: name, stores, and location.
 const storeOwners = [
     {
       name: 'Danny Shavez',
       stores: 1,
       location: "AZ",
     },
-    {
-      name: 'Danny Shavez',
-      stores: 1,
-      location: "NM",
-    },
-    {
-      name: 'Danny Shavez',
-      stores: 1,
-      location: "NM",
-    },
-    {
-      name: 'Danny Shavez',
-      stores: 1,
-      location: "NM",
-    },
+    // More store owners...
   ];
   
+  // Calculates the total number of stores all owners have.
   const listNumberOfStores = function () {
+    // Total of 0 stores.
+    let totalLocations = 0;
+    
+    // Each store owner...
     for (let i = 0; i < storeOwners.length; i++) {
-      let totalLocations = totalLocaions + storeOwners.stores;
+      // Adding the number of stores this owner has to our total.
+      totalLocations = totalLocations + storeOwners[i].stores;
     }
-    return i;
+    
+    // Return the total number of stores.
+    return totalLocations;
   };
   
-  let locations = listNumberOfStores;
+  // Call function to get the total number of stores and store the result in a variable.
+  let locations = listNumberOfStores();
   
+  // This function logs the total number of stores to the console.
   function tellMeMyStores() {
     console.log('Hey, can you tell me how many stores you have?');
     if (locations > 0) {
@@ -37,14 +34,20 @@ const storeOwners = [
     }
   }
   
+  // This function logs each store owner's name and location to the console.
   function hasStore() {
+    // Go through each store owner...
     for (let i = 0; i < storeOwners.length; i++) {
-      let person = storeOwners.name;
-      let location = storeOwners.location();
-      console.log('Yes, {person} has one in {this.location}');
+      // ...and get their name and location.
+      let person = storeOwners[i].name;
+      let location = storeOwners[i].location;
+      
+      // Log a message to the console using these details.
+      console.log('Yes, ' + person + ' has one in ' + location);
     }
-  
   }
   
+  // Call the two functions.
   tellMeMyStores();
   hasStore();
+  
